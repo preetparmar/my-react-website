270b1a9e6b08bf11940557873865f338acb4d9c0
Preet Parmar <preetparmar@outlook.com>
2021-10-29 16:09:08 +0530

    Added Scroll to Top functionality

--- build/static/js/11.d464c8e7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppreet-parmar"]=this["webpackJsonppreet-parmar"]||[]).push([[11],{79:function(t,e,n){"use strict";n.r(e);var i=n(0),c=n(26),r=n(1);e.default=function(t){return Object(i.useEffect)((function(){window.location.assign(t.link)}),[]),Object(r.jsxs)("div",{children:[window.location.href="",";",Object(r.jsx)(c.a,{})]})}}}]);
-//# sourceMappingURL=11.d464c8e7.chunk.js.map
