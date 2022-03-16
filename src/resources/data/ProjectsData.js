f6cceba304a83f5ada7b5c1954dddec42c373994
Preet Parmar <preetparmar@outlook.com>
2022-03-16 20:08:50 +0530

    Updated My Website Project
    Added the GitHub Repository link to the website code.

--- src/resources/data/ProjectsData.js
+++ src/resources/data/ProjectsData.js
@@ -91,7 +91,7 @@ const myWebsite = [
     image: null,
     detail:
       "During the pandemic (COVID-19), I decided to create a single page resume and publish it to the web. This small idea led me to create the current website. This is my first attempt at creating a website so feel free to inspect my code and reach me at my email with any feedback.",
-    link: null,
+    link: "https://github.com/preetparmar/my-react-website",
   },
 ];
 
