8da5753931d0609896292c1b065d431b2b285f38
Preet Parmar <preetparmar@outlook.com>
2022-04-01 10:55:20 +0530

    Update HomeData.js

--- src/resources/data/HomeData.js
+++ src/resources/data/HomeData.js
@@ -14,11 +14,11 @@ const HomeData = {
   thought:
     "The mind that opens to a new idea never returns to its original size",
   details: [
-    "My expertise lies in Data Analytics/Data tools: mining, cleaning, transformation, and visualizations.",
+    "My expertise lies in Data Analytics and Data tools: mining, cleaning, transformation, and visualizations.",
     "I love simplifying and perfecting things which translates into my way of working.",
     "Before this, I completed my M.S. degree in Engineering Management from Northeastern University, Boston where I gained experience in both Management as well as Data Analytics concepts.",
     "I have built multiple data processes to collect, organize, transform and visualize the data.",
-    "I have also been responsible to lead a team and mentor them in various emerging data tools and designing a roadmap for learning",
+    "I have also been responsible to lead a team and mentor them in various emerging data tools and designing a roadmap for learning.",
     "I love solving complex problems and finding the optimal solution for them, mainly because it provides me with an opportunity to widen my skill set and learn something new.",
     "The thing which keeps me going is my hunger to learn new things. This has led me to work on various projects outside my skill set, one prime example is this website.",
     "I love anything tech-related, any new piece of technology, software, game, or a new language to learn, just sign me up for it.",
